a600c117553fcf56b15486b32b20176eb30d7834
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-07-19 18:23:25 +0200

    switch to test cases for equality

--- config.js
+++ config.js
@@ -1,17 +1,28 @@
 import fullTestConfig from './fullTestConfig.json' with {type: 'json'};
 
-const prefix = 'test/language/literals/'
-const prob = 0.2
+const predicate = path => 
+  path.startsWith('test/language/expressions/does-not-equals/')
+  || path.startsWith('test/language/expressions/equals/')
+  || path.startsWith('test/language/expressions/greater-than-or-equal/')
+  || path.startsWith('test/language/expressions/less-than-or-equal/')
+  || path.startsWith('test/language/expressions/strict-does-not-equals/')
+  || path.startsWith('test/language/expressions/strict-equals/');
+
+const prob = 0.4
 
 const filteredTestCases = []
 console.log(fullTestConfig)
 for (const tc of fullTestConfig.testCases) {
-  if (tc.startsWith(prefix) && Math.random() < prob) {
+  if (predicate(tc) && Math.random() < prob) {
     filteredTestCases.push(tc)
   }
 }
 
 const output = { testCases: filteredTestCases }
 Deno.writeTextFile('./testConfig.json', JSON.stringify(output))
+
+const nSel = filteredTestCases.length
+const nTotal = fullTestConfig.testCases.length
+console.log(`selected ${nSel} out of ${nTotal} test cases`)
 
 

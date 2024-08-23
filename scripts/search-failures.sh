0f6f43b453a982ddb4b2348ab9716c47e84859b5
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-08-23 21:53:55 +0200

    search-failures: don't show 'total' on last line

--- scripts/search-failures.sh
+++ scripts/search-failures.sh
@@ -6,6 +6,5 @@ awk -F'\t' -v pattern="$1" '
 $1 == "case" {testcase = $2; pctx=0}
 $1 == "error" && $2 ~ pattern { total++; print testcase ": " $2; pctx=1 }
 pctx && $1 == "ectx" { print "\t", $2 }
-END {print "total",total}
 '
 

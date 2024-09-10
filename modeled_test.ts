da54182696a1b30e263a3dfccace767e5864dc0b
Sebastiano Barrera <sebastiano.barrera@gmail.com>
2024-09-10 23:20:35 +0200

    modeled_test: refactor

--- modeled_test.ts
+++ modeled_test.ts
@@ -25,91 +25,105 @@ function findPatternJS(patternJS: string): acorn.Pattern {
 	const ast: acorn.Program = acorn.parse(patternJS, { ecmaVersion: 2020 });
 	const node1 = ast.body[0];
 	if (
-		node1.type !== 'VariableDeclaration'
-		|| node1.declarations.length !== 1
-	) throw new Error('invalid test js: not in the form: [var x = _]');
+		node1.type !== "VariableDeclaration" ||
+		node1.declarations.length !== 1
+	) throw new Error("invalid test js: not in the form: [var x = _]");
 	return node1.declarations[0].id;
 }
 
 function bindingPatternJS(patternJS: string, value: M.JSValue): BindingSet {
 	const patternAST = findPatternJS(patternJS);
-	return bindingPattern(patternAST, value);
+	const bset = new Map<string, M.JSValue>();
+	bindingPattern(patternAST, value, (name, value) => bset.set(name, value));
+	return bset;
 }
 
-type BindingSet = { [newIdent: string]: M.JSValue };
+type BindingSet = Map<string, M.JSValue>;
 
 function bindingPattern(
 	pattern: acorn.Pattern,
 	value: M.JSValue,
-	bset?: BindingSet,
-): BindingSet {
-	bset ??= {};
-
+	bind: (name: string, value: M.JSValue) => void,
+) {
 	if (pattern.type === "Identifier") {
 		const ident = pattern.name;
-		bset[ident] = value;
+		bind(ident, value);
 	} else if (pattern.type === "ObjectPattern") {
-		if (value instanceof M.VMObject) {
-			const count = pattern.properties.length;
-			if (count === 0)
-				return bset;
-
-			const last = pattern.properties[count - 1];
-			const restObj: undefined | M.VMObject
-				= last.type === "RestElement" 
-				? value.shallowCopy()
-				: undefined;
-
-			for (const prop of pattern.properties) {
-				if (prop.type === "Property") {
-					if (prop.key.type === "Identifier") {
-						const key = prop.key.name;
-						const subValue = value.getProperty(key) ?? {type: 'undefined'};
-						bindingPattern(prop.value, subValue, bset);
-						if (restObj !== undefined) {
-							restObj.deleteProperty(key);
-						}
-					} else {
-						M._CV?.throwError("TypeError", "unsupported syntax for object pattern key: " + prop.key.type);
-					}
-				} else if (prop.type === "RestElement") {
-					if (restObj instanceof M.VMObject) {
-						bindingPattern(prop.argument, restObj, bset);
-					} else {
-						throw new Error('there should be an object here!');
-					}
-
-				} else {
-					const x: never = prop;
-				}
-			}
-		} else {
-			M._CV?.throwError(
+		const object = M._CV?.coerceToObject(value);
+		if (object === undefined) {
+			return M._CV!.throwError(
 				"TypeError",
-				`type ${value.type} can't be matched to object pattern`,
+				"object pattern can't be matched with value that can't be coerced to object",
 			);
 		}
 
-	} else if (pattern.type === "AssignmentPattern") {
-		if (value.type === 'undefined') {
-			const fallbackValue = M._CV!.evalExpr(pattern.right);
-			bindingPattern(pattern.left, fallbackValue, bset);
-		} else {
-			bindingPattern(pattern.left, value, bset);
+		const count = pattern.properties.length;
+		if (count === 0) {
+			return;
 		}
 
+		const last = pattern.properties[count - 1];
+		const restObj: undefined | M.VMObject = last.type === "RestElement"
+			? object.shallowCopy()
+			: undefined;
+
+		for (const prop of pattern.properties) {
+			if (prop.type === "Property") {
+				if (prop.key.type === "Identifier") {
+					const key = prop.key.name;
+					const subValue = object.getProperty(key) ??
+						{ type: "undefined" };
+					bindingPattern(prop.value, subValue, bind);
+					if (restObj !== undefined) {
+						restObj.deleteProperty(key);
+					}
+				} else {
+					return M._CV!.throwError(
+						"TypeError",
+						"unsupported syntax for object pattern key: " +
+							prop.key.type,
+					);
+				}
+			} else if (prop.type === "RestElement") {
+				if (restObj instanceof M.VMObject) {
+					bindingPattern(prop.argument, restObj, bind);
+				} else {
+					throw new Error("there should be an object here!");
+				}
+			} else {
+				const x: never = prop;
+			}
+		}
+	} else if (pattern.type === "ArrayPattern") {
+		const count = pattern.elements.length;
+		if (count === 0) {
+			return;
+		}
+		// const last = pattern.elements[count - 1];
+
+		for (let i = 0; i < count; i++) {
+			const elmPat = pattern.elements[i];
+		}
+
+		// TODO
+	} else if (pattern.type === "AssignmentPattern") {
+		if (value.type === "undefined") {
+			const fallbackValue = M._CV!.evalExpr(pattern.right);
+			bindingPattern(pattern.left, fallbackValue, bind);
+		} else {
+			bindingPattern(pattern.left, value, bind);
+		}
 	} else {
 		throw new Error("unsupported/invalid pattern type: " + pattern.type);
 	}
-
-	return bset;
 }
 
 Deno.test("simple identifier", () => {
 	using _ = useNewVM();
 	const value: M.JSValue = { type: "number", value: 123.0 };
 	const bset = bindingPatternJS("var x", value);
-	expect(bset).toMatchObject({ x: value });
+	expect(bset.size).toBe(1);
+	expect(bset.get("x")).toBe(value);
 });
 
 Deno.test("empty object <- object", () => {
@@ -121,7 +135,7 @@ Deno.test("empty object <- object", () => {
 Deno.test("empty object <- non object", () => {
 	expect(() => {
 		using _ = useNewVM();
-		const value: M.JSValue = { type: "number", value: 123.0 };
+		const value: M.JSValue = { type: "null" };
 		bindingPatternJS("var {} = _", value);
 	}).toThrow(M.ProgramException);
 });
@@ -132,26 +146,39 @@ Deno.test("object: single property", () => {
 	const value = new M.VMObject();
 	value.setProperty("lol", num);
 	const bset = bindingPatternJS("var {lol} = _", value);
-	expect(bset.lol).toBe(num);
+	expect(bset.get('lol')).toBe(num);
 });
 
 Deno.test("object: single property, absent", () => {
 	using _ = useNewVM();
 	const value = new M.VMObject();
 	const bset = bindingPatternJS("var {lol} = _", value);
-	expect(bset.lol).toMatchObject({type: 'undefined'});
+	expect(bset.get("lol")).toMatchObject({ type: "undefined" });
 });
 
 Deno.test("object: single property, default", () => {
 	using _ = useNewVM();
 	const value = new M.VMObject();
 	const bset = bindingPatternJS("var {lol = 99} = _", value);
-	expect(bset.lol).toMatchObject({type: 'number', value: 99.0});
+	expect(bset.get("lol")).toMatchObject({ type: "number", value: 99.0 });
 });
 
 Deno.test("pattern: object, single property", () => {
 	const ast = findPatternJS("var {lol} = _");
 	expect(ast.type).toBe("ObjectPattern");
+});
+
+Deno.test("pattern: object, coerce", () => {
+	using _ = useNewVM();
+
+	const value: M.JSValue = { type: "number", value: 123.0 };
+	const bset = bindingPatternJS("var {valueOf, toString} = _", value);
+
+	console.log(bset);
+
+	expect(bset.size).toBe(2);
+	expect(bset.get("valueOf")?.type).toBe("function");
+	expect(bset.get("toString")?.type).toBe("function");
 });
 
 Deno.test("pattern: object, mix", () => {
@@ -160,17 +187,13 @@ Deno.test("pattern: object, mix", () => {
 	const num: M.JSValue = { type: "number", value: 123.0 };
 	const value = new M.VMObject();
 	value.setProperty("lol", num);
-	value.setProperty("rofl", {type: "string", value: "something something"});
+	value.setProperty("rofl", { type: "string", value: "something something" });
 
 	const bset = bindingPatternJS("var {asd = 99, lol} = _", value);
 
-	const keys = Object.keys(bset);
-	keys.sort();
-	expect(keys).toHaveLength(2);
-	expect(keys).toContain('asd');
-	expect(keys).toContain('lol');
-	expect(bset.asd).toMatchObject({type: 'number', value: 99.0});
-	expect(bset.lol).toMatchObject({type: 'number', value: 123.0});
+	expect(bset.size).toBe(2);
+	expect(bset.get("asd")).toMatchObject({ type: "number", value: 99.0 });
+	expect(bset.get("lol")).toMatchObject({ type: "number", value: 123.0 });
 });
 
 Deno.test("pattern: object, ...rest", () => {
@@ -185,15 +208,36 @@ Deno.test("pattern: object, ...rest", () => {
 
 	const bset = bindingPatternJS("var {a, b, ...more} = _", value);
 
-	const keys = Object.keys(bset);
-	keys.sort();
-	expect(keys).toHaveLength(3);
-	expect(keys).toContain('a');
-	expect(keys).toContain('b');
-	expect(keys).toContain('more');
+	expect(bset.size).toBe(3);
+	expect(bset.get("a")).toMatchObject({ type: "number", value: 1.0 });
+	expect(bset.get("b")).toMatchObject({ type: "number", value: 2.0 });
 
-	expect(bset.a).toMatchObject({type: 'number', value: 1.0});
-	expect(bset.b).toMatchObject({type: 'number', value: 2.0});
+	const more = <M.VMObject> bset.get("more");
+	expect(more).toBeInstanceOf(M.VMObject);
+
+	const moreKeys = [...more.getOwnPropertyNames()];
+	moreKeys.sort();
+	expect(moreKeys[0]).toBe('c');
+	expect(moreKeys[1]).toBe('d');
+	expect(moreKeys[2]).toBe('e');
 });
 
+Deno.test("pattern: array empty", () => {
+	using _ = useNewVM();
 
+	const value = new M.VMArray();
+	const bset = bindingPatternJS("var [] = _", value);
+	expect(bset).toMatchObject({});
+});
+
+Deno.test("pattern: array single element", () => {
+	using _ = useNewVM();
+
+	const num: M.JSValue = { type: "number", value: 8234135.1 };
+	const value = new M.VMArray();
+	value.setIndex(0, num);
+	const bset = bindingPatternJS("var [x] = _", value);
+
+	expect(bset.size).toBe(1);
+	expect(bset.get("x")).toBe(num);
+});
